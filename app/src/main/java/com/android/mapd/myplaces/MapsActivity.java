1df2318625010776fe9adbe1820d90e850746a8c
Rodrigo Vieira <digovs@gmail.com>
2018-12-08 18:13:24 -0500

    Adding floating action buttons

--- app/src/main/java/com/android/mapd/myplaces/MapsActivity.java
+++ app/src/main/java/com/android/mapd/myplaces/MapsActivity.java
@@ -1,28 +1,48 @@
 package com.android.mapd.myplaces;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
+import com.github.clans.fab.FloatingActionButton;
+import com.github.clans.fab.FloatingActionMenu;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, View.OnClickListener {
 
     private GoogleMap mMap;
-    @BindView(R.id.searchEditText)
-    EditText searchEditText;
 
-    @BindView(R.id.searchButton)
-    Button searchButton;
+    @BindView(R.id.restaurantFAB)
+    FloatingActionButton restaurantFAB;
+
+    @BindView(R.id.movieTheaterFAB)
+    FloatingActionButton movieTheaterFAB;
+
+    @BindView(R.id.barFAB)
+    FloatingActionButton barFAB;
+
+    @BindView(R.id.floatingActionMenu)
+    FloatingActionMenu floatingActionMenu;
+
+    int PLACE_PICKER_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +53,41 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        initFABButtons();
+
+    }
+
+    private void initFABButtons() {
+        restaurantFAB.setOnClickListener(this);
+        movieTheaterFAB.setOnClickListener(this);
+        barFAB.setOnClickListener(this);
+    }
+
+    private void buildPLacePicker() {
+        PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+        builder.setLatLngBounds(LatLngBounds.builder()
+                .include(new LatLng(43.670233, -79.372494))
+                .include(new LatLng(43.970233, -79.072494))
+                .build());
+        try {
+            startActivityForResult(builder.build(this), PLACE_PICKER_REQUEST);
+        } catch (GooglePlayServicesRepairableException e) {
+            e.printStackTrace();
+        } catch (GooglePlayServicesNotAvailableException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_PICKER_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlacePicker.getPlace(this, data);
+                String toastMsg = String.format("Place: %s", place.getName());
+                Toast.makeText(this, toastMsg, Toast.LENGTH_LONG).show();
+                floatingActionMenu.close(true);
+            }
+        }
     }
 
 
@@ -53,5 +108,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng toronto = new LatLng(43.670233, -79.372494);
         mMap.addMarker(new MarkerOptions().position(toronto).title("Marker in Toronto"));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(toronto, 12f));
+    }
+
+    @Override
+    public void onClick(View v) {
+        buildPLacePicker();
     }
 }
