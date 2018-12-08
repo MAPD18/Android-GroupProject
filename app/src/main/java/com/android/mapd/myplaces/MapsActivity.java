4159cef4ecffc52b5b1b656ad17ce621897ef177
Rodrigo Vieira <digovs@gmail.com>
2018-12-08 15:43:30 -0500

    Adding a Search Box

--- app/src/main/java/com/android/mapd/myplaces/MapsActivity.java
+++ app/src/main/java/com/android/mapd/myplaces/MapsActivity.java
@@ -39,8 +39,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng toronto = new LatLng(43.670233, -79.372494);
+        mMap.addMarker(new MarkerOptions().position(toronto).title("Marker in Toronto"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(toronto, 12f));
     }
 }
